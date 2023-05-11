272c4d63059ef3833fca5cbc6d778854beb9e580
ian balijawa <ianbalijawa16@gmail.com>
2023-05-11 20:59:25 +0300

    build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Navigate, useNavigate, useRoutes } from 'react-router-dom';
-import { Suspense, lazy, useEffect } from 'react';
+import { Suspense, useEffect } from 'react';
 
 import { Spinner } from '@chakra-ui/react';
 import ComplaintList from './Pages/ComplaintList';
@@ -15,37 +15,6 @@ import HodComplaint from './Pages/HodComplaint';
 import ComplaintStatus from './Pages/Status';
 import Reviews from './Pages/Reviews';
 import Lecturer from './Pages/Lecturer';
-
-const Loadable = (Component: React.FC) => (props: any) =>
-    (
-        <Suspense
-            fallback={
-                <div
-                    style={{
-                        width: '100%',
-                        margin: '20rem auto',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignContent: 'center',
-                    }}
-                >
-                    <Spinner size="xs" />
-                </div>
-            }
-        >
-            <Component {...props} />
-        </Suspense>
-    );
-// const Home = Loadable(lazy(() => import('./Pages/Home')));
-// const Login = Loadable(lazy(() => import('./Pages/Login')));
-// const Register = Loadable(lazy(() => import('./Pages/Register')));
-// const Summary = Loadable(lazy(() => import('./Pages/Summary')));
-// const Complaint = Loadable(lazy(() => import('./Pages/Complaint')));
-// const Profile = Loadable(lazy(() => import('./Pages/Profile')));
-// const ChangePassword = Loadable(lazy(() => import('./Pages/ChangePassword')));
-// const Faqs = Loadable(lazy(() => import('./Pages/Faqs')));
-// const HodComplaint = Loadable(lazy(() => import('./Pages/HodComplaint')));
-// const ComplaintStatus = Loadable(lazy(() => import('./Pages/Status')));
 
 export default () => {
     return useRoutes([
@@ -124,11 +93,12 @@ const NotFound = () => {
             style={{
                 display: 'flex',
                 height: '100vh',
+                fontSize: '2em',
                 justifyContent: 'center',
                 alignItems: 'center',
             }}
         >
-            Page Not Found
+            404
         </div>
     );
 };
