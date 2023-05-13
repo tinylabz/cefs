0310525b1e30788b32efebbb54327ca91340faf7
ian balijawa <ianbalijawa16@gmail.com>
2023-05-13 21:10:24 +0300

    disable force consistent casing in file name

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import ComplaintList from './Pages/ComplaintList';
 import Complaints from './Pages/Complaints';
 import Home from './Pages/Home';
-import Signin from './Pages/Signin';
+import Signin from './Pages/signin';
 import Register from './Pages/Register';
 import Summary from './Pages/Summary';
 import Profile from './Pages/Profile';
