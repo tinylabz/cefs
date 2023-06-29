320fed2539eaa60762c806cfc57a283a681231a6
ian balijawa <ianbalijawa16@gmail.com>
2023-06-29 19:16:55 +0300

    make urls conditional base on env

--- src/config/axios-config.ts
+++ src/config/axios-config.ts
@@ -1,5 +1,18 @@
 import instance from 'axios';
 
 export const axios = instance.create({
-    baseURL: 'https://cefs.onrender.com/api',
+    baseURL:
+        process.env.NODE_ENV === 'production'
+            ? 'https://cefs.onrender.com/api'
+            : 'http://localhost:4000/api',
 });
+
+export const baseURL =
+    process.env.NODE_ENV === 'production'
+        ? 'https://cefs.onrender.com/api'
+        : 'http://localhost:4000/api';
+
+export const baseURLUpload =
+    process.env.NODE_ENV === 'production'
+        ? 'https://cefs.onrender.com/'
+        : 'http://localhost:4000/';
